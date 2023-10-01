a350ad469fe7b9b80dc310a3b34b35e1a2143e8e
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-10-02 00:38:59 +0530

    This Shall Deploy The Site.

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,7 +5,7 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const maxDuration = 10; // This function can run for a maximum of 5 seconds
 
 export const dynamic='force-dynamic';
 
