42df47e47a37b12d6f3837ab0eaac5b1898023b0
IDY12345 <106066822+IDY12345@users.noreply.github.com>
2023-12-18 15:38:02 +0530

    Changes To Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import Navbar from '@/Components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter,Space_Grotesk } from 'next/font/google'
+import { Inter, Space_Grotesk } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const space_Grotesk=Space_Grotesk({
-  subsets: ['latin'],weight:['300','400','500','600','700']
+const space_Grotesk = Space_Grotesk({
+  subsets: ['latin'], weight: ['300', '400', '500', '600', '700']
 })
 
 export const metadata: Metadata = {
@@ -23,10 +23,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className='max-w-10xl mx-auto'>
-        <Navbar />
-        {children}
+          <Navbar />
+          {children}
         </main>
-        </body>
+      </body>
     </html>
   )
 }
